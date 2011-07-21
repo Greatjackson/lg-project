4e4cf0d2041794d189f0cbd82daa4fb154f026cf
Jackson <308584349@163.com>
2011-07-21 11:17:37 +0800

    vim skill Ctrl n
    it will search current file and all included file

--- hello.c
+++ hello.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
-
+#define HOW_MANY_TIMES_TO_SAY_HELLO 100
 int main(int argc, const char *argv[])
 
 	
 
 {
         int i;
-	for (i=0;i<9;i++)
+	for (i=0;i<HOW_MANY_TIMES_TO_SAY_HELLO;i++)
 	{
 	printf ("hello!\n");
 	}	
